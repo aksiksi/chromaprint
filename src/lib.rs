5cb683477da18c3462e601462427fdfe17f8132a
Assil Ksiksi <cyph0nik@gmail.com>
2022-06-22 01:42:13 -0400

    Refactor Fingerprint to use typestate pattern
    Idea is to properly encode the fingerprint type at compile-time.
    
    See: http://cliffle.com/blog/rust-typestate/

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
 extern crate chromaprint_sys;
 extern crate thiserror;
 
-use std::ffi::{c_void, CStr};
-
 use chromaprint_sys::*;
-use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("operation failed")]
     OperationFailed,
@@ -43,76 +40,72 @@ pub enum Channels {
 }
 
 #[derive(Debug)]
-pub enum Fingerprint<'a> {
-    Base64(&'a str),
-    Raw(&'a [u32]),
-    Hash(u32),
+pub struct Fingerprint<F: FingerprintType> {
+    inner: F,
 }
 
-impl<'a> Fingerprint<'a> {
-    #[inline]
-    pub fn base64(&self) -> Option<&'a str> {
-        match *self {
-            Fingerprint::Base64(v) => Some(v),
-            _ => None,
-        }
-    }
-
-    #[inline]
-    pub fn raw(&self) -> Option<&'a [u32]> {
-        match *self {
-            Fingerprint::Raw(v) => Some(v),
-            _ => None,
-        }
-    }
-
-    #[inline]
-    pub fn hash(&self) -> Option<u32> {
-        match *self {
-            Fingerprint::Hash(v) => Some(v),
-            _ => None,
-        }
-    }
-
-    /// Convert a raw fingerprint to a hash.
-    ///
-    /// If this method is called on a non-raw fingerprint, an `InvalidArgument` error will be returned.
-    pub fn to_hash(&self) -> Result<Fingerprint<'_>> {
-        if let Some(raw) = self.raw() {
-            let mut hash: u32 = 0;
-            let rc =
-                unsafe { chromaprint_hash_fingerprint(raw.as_ptr(), raw.len() as i32, &mut hash) };
-            if rc != 1 {
-                return Err(Error::OperationFailed);
-            }
-            return Ok(Self::Hash(hash));
-        }
-
-        Err(Error::InvalidArgument(
-            "raw fingerprint required".to_string(),
-        ))
-    }
+pub trait FingerprintType {
+    // Hacky way to circumvent this: https://doc.rust-lang.org/error-index.html#E0366
+    fn drop(&mut self) {}
 }
 
-impl<'a> Drop for Fingerprint<'a> {
+#[derive(Debug)]
+pub struct Base64<'a>(&'a str);
+#[derive(Debug)]
+pub struct Raw<'a>(&'a [u32]);
+#[derive(Debug)]
+pub struct Hash(u32);
+
+impl<'a> FingerprintType for Base64<'a> {
     fn drop(&mut self) {
-        let mut ptr: Option<*mut c_void> = None;
-        match self {
-            Fingerprint::Base64(s) => ptr = Some(s.as_ptr() as *mut c_void),
-            Fingerprint::Raw(s) => ptr = Some(s.as_ptr() as *mut c_void),
-            Fingerprint::Hash(_) => (),
-        }
-        if let Some(ptr) = ptr {
-            unsafe { chromaprint_dealloc(ptr) };
-        }
+        unsafe { chromaprint_dealloc(self.0.as_ptr() as *mut std::ffi::c_void) };
     }
 }
 
-#[derive(Clone, Copy, Debug)]
-pub enum FingerprintKind {
-    Base64,
-    Raw,
-    Hash,
+impl<'a> FingerprintType for Raw<'a> {
+    fn drop(&mut self) {
+        unsafe { chromaprint_dealloc(self.0.as_ptr() as *mut std::ffi::c_void) };
+    }
+}
+
+impl FingerprintType for Hash {}
+
+impl<'a> Fingerprint<Base64<'a>> {
+    pub fn get(&self) -> &'a str {
+        self.inner.0
+    }
+}
+
+impl<'a> Fingerprint<Raw<'a>> {
+    pub fn get(&self) -> &'a [u32] {
+        self.inner.0
+    }
+}
+
+impl Fingerprint<Hash> {
+    pub fn get(&self) -> u32 {
+        self.inner.0
+    }
+}
+
+impl<'a> TryFrom<Fingerprint<Raw<'a>>> for Fingerprint<Hash> {
+    type Error = Error;
+    fn try_from(raw: Fingerprint<Raw<'a>>) -> Result<Self> {
+        let mut hash: u32 = 0;
+        let data = raw.get();
+        let rc =
+            unsafe { chromaprint_hash_fingerprint(data.as_ptr(), data.len() as i32, &mut hash) };
+        if rc != 1 {
+            return Err(Error::OperationFailed);
+        }
+        return Ok(Fingerprint { inner: Hash(hash) });
+    }
+}
+
+impl<F: FingerprintType> Drop for Fingerprint<F> {
+    fn drop(&mut self) {
+        self.inner.drop();
+    }
 }
 
 pub struct Context {
@@ -133,12 +126,10 @@ impl Context {
     pub fn start(&mut self, sample_rate: u32, num_channels: Channels) -> Result<()> {
         let sample_rate = sample_rate as i32;
         let num_channels = num_channels as i32;
-
         let rc = unsafe { chromaprint_start(self.ctx, sample_rate, num_channels) };
         if rc != 1 {
             return Err(Error::OperationFailed);
         }
-
         Ok(())
     }
 
@@ -158,44 +149,39 @@ impl Context {
         Ok(())
     }
 
-    pub fn get_fingerprint<'a>(&'a mut self, kind: FingerprintKind) -> Result<Fingerprint<'_>> {
-        let fingerprint: Fingerprint;
-        match kind {
-            FingerprintKind::Base64 => {
-                let mut out_ptr = std::ptr::null::<*const libc::c_char>() as *mut libc::c_char;
-                let rc = unsafe {
-                    chromaprint_get_fingerprint(self.ctx, std::ptr::addr_of_mut!(out_ptr))
-                };
-                if rc != 1 {
-                    return Err(Error::OperationFailed);
-                }
-                let s = unsafe { CStr::from_ptr(out_ptr as *const libc::c_char) }.to_str();
-                if s.is_err() {
-                    return Err(Error::InvalidFingerprintString(s.err().unwrap()));
-                }
-                fingerprint = Fingerprint::Base64(s.unwrap());
-            }
-            FingerprintKind::Raw => {
-                let mut data_ptr = std::ptr::null::<*const u32>() as *mut u32;
-                let mut size: i32 = 0;
-                let rc =
-                    unsafe { chromaprint_get_raw_fingerprint(self.ctx, &mut data_ptr, &mut size) };
-                if rc != 1 {
-                    return Err(Error::OperationFailed);
-                }
-                let s = unsafe { std::slice::from_raw_parts_mut(data_ptr, size as usize) };
-                fingerprint = Fingerprint::Raw(s);
-            }
-            FingerprintKind::Hash => {
-                let mut hash: u32 = 0;
-                let rc = unsafe { chromaprint_get_fingerprint_hash(self.ctx, &mut hash) };
-                if rc != 1 {
-                    return Err(Error::OperationFailed);
-                }
-                fingerprint = Fingerprint::Hash(hash);
-            }
+    pub fn get_fingerprint_base64<'a>(&'a mut self) -> Result<Fingerprint<Base64>> {
+        let mut out_ptr = std::ptr::null::<*const libc::c_char>() as *mut libc::c_char;
+        let rc = unsafe { chromaprint_get_fingerprint(self.ctx, std::ptr::addr_of_mut!(out_ptr)) };
+        if rc != 1 {
+            return Err(Error::OperationFailed);
         }
-        Ok(fingerprint)
+        let s = unsafe { std::ffi::CStr::from_ptr(out_ptr as *const libc::c_char) }.to_str();
+        if s.is_err() {
+            return Err(Error::InvalidFingerprintString(s.err().unwrap()));
+        }
+        Ok(Fingerprint {
+            inner: Base64(s.unwrap()),
+        })
+    }
+
+    pub fn get_fingerprint_raw<'a>(&'a mut self) -> Result<Fingerprint<Raw>> {
+        let mut data_ptr = std::ptr::null::<*const u32>() as *mut u32;
+        let mut size: i32 = 0;
+        let rc = unsafe { chromaprint_get_raw_fingerprint(self.ctx, &mut data_ptr, &mut size) };
+        if rc != 1 {
+            return Err(Error::OperationFailed);
+        }
+        let s = unsafe { std::slice::from_raw_parts_mut(data_ptr, size as usize) };
+        Ok(Fingerprint { inner: Raw(s) })
+    }
+
+    pub fn get_fingerprint_hash(&mut self) -> Result<Fingerprint<Hash>> {
+        let mut hash: u32 = 0;
+        let rc = unsafe { chromaprint_get_fingerprint_hash(self.ctx, &mut hash) };
+        if rc != 1 {
+            return Err(Error::OperationFailed);
+        }
+        Ok(Fingerprint { inner: Hash(hash) })
     }
 
     pub fn clear_fingerprint(&mut self) -> Result<()> {
@@ -209,9 +195,7 @@ impl Context {
 
 impl Default for Context {
     fn default() -> Self {
-        let algorithm = Algorithm::default();
-        let ctx = unsafe { chromaprint_new(algorithm as i32) };
-        Self { ctx, algorithm }
+        Self::new(Algorithm::default())
     }
 }
 
